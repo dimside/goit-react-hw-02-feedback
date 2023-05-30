a78e6a4db5229deda6f2e4b5c9622848ce2ae945
dimside <dimside29@gmail.com>
2023-05-30 03:15:48 +0300

    completed-goit-react-hw-02-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+
+const FEEDBACK_OPTIONS = ['good', 'neutral', 'bad'];
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, feedbackQuantity) => {
+      return acc + feedbackQuantity;
+    }, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={FEEDBACK_OPTIONS}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,26 @@
+import { nanoid } from 'nanoid';
+import css from './Statistics.module.css';
+
+export const Statistics = props => {
+  return (
+    <div>
+      <ul className={css.statistic_list}>
+        {Object.entries(props).map(statistic => (
+          <li key={nanoid()}>
+            <p>
+              {statistic[0] === 'positivePercentage'
+                ? 'Positive feedback'
+                : statistic[0]}
+            </p>
+            <span>
+              :
+              {statistic[0] === 'positivePercentage'
+                ? `${statistic[1]}%`
+                : statistic[1]}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
